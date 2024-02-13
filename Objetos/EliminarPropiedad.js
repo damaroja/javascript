dc3a160156dcf11f01a791dfa49edeaa96319288
damaroja <ildefonsoarrecife3@hotmail.com>
2024-02-13 12:12:35 +0000

    Create EliminarPropiedad.js

---
+++ Objetos/EliminarPropiedad.js
@@ -0,0 +1,14 @@
+
+//Tenemos un objeto y queremos eliminar ela propiedad age,
+// creamos dos constantes, age que es lo que queremos eliminar
+// y noAge que es el objeto que se devuelve
+
+const obj = {
+  name: 'John',
+  lastname: 'Doe',
+  age: 45,
+  id: 'U-9472000013',
+  location: 'Norway',
+};
+
+const { age, ...noAge } = obj;
