443c12385d1c8bf500ff9e163984ff10b21ade6d
damaroja <ildefonsoarrecife3@hotmail.com>
2024-03-04 16:44:30 +0000

    Create SumarCantidadesArray.js

---
+++ Array/SumarCantidadesArray.js
@@ -0,0 +1,17 @@
+
+
+const cars = [
+    { make: 'Toyota', model: 'Camry', year: 2020, mileage: 30800 },
+    { make: 'Honda', model: 'Civic', year: 2019, mileage: 32000 },
+    { make: 'Chevrolet', model: 'Impala', year: 2021, mileage: 17500 },
+    { make: 'Audi', model: 'R8', year: 2020, mileage: 13000 },
+    { make: 'Tesla', model: 'Model 3', year: 2018, mileage: 50000 },
+  ];
+
+
+//Queremos obtener la suma de todos los mileages:
+
+const sumaMileage = (cars) => {
+  return cars.reduce((acc, car) => acc + car.mileage, 0);
+}
+
