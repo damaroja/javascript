2a2138487c2c8a2aaeaa030a1d5f6171aa47d0ee
damaroja <ildefonsoarrecife3@hotmail.com>
2024-03-04 16:31:32 +0000

    Update ipv4Format.js

--- DireccionesIP/ipv4Format.js
+++ DireccionesIP/ipv4Format.js
@@ -1,6 +1,23 @@
 
 
 
+
+
+/**
+ * Verifies if a given string is valid.
+ * @param {string} str - The string to be verified.
+ * @throws {Error} If the string is undefined, empty, or not a string.
+ */
+
+
+const verifyString = (str) => {
+    if(str === undefined) throw new Error('Undefined string');
+    if(typeof str !== 'string') throw new Error('Not a string');
+    if(str === '') throw new Error('Empty string');
+}
+
+
+
 /**
  * Checks if the given input string is a valid IPv4 address.
  *
@@ -12,13 +29,47 @@
 //funcion que comprueba si la direccion es ipv4
 
 function isIPv4Address(inputString) {
-    if(inputString.trim() === '') throw new Error('La cadena no puede estar vacia');
-    if(inputString === null) throw new Error('La cadena no puede ser nula');
-    if(inputString === undefined) throw new Error('La cadena no puede ser indefinida');
-    let arr = inputString.split('.');
-    if (arr.length !== 4) return false;
-    arr.every((el) => el >= 0 && el <= 255 && el !== '' && !isNaN(el))
-    return true;
+  verifyString(inputString);
+  let arr = inputString.split(".");
+  if (arr.length !== 4) return false;
+  return arr.every((element) => {
+    const el = parseInt(element);
+    return (
+      typeof el === "number" &&
+      el >= 0 &&
+      el <= 255 &&
+      el.toString() === element
+    );
+  });
 }
+
+module.exports = { isIPv4Address };
+
+
+
+//************************************************
+
+
+
+const { isIPv4Address } = require('./app');
+
+describe('isIPv4Address', () => {
+  test('should return true for valid IPv4 address', () => {
+    expect(isIPv4Address('127.0.0.1')).toBe(true);
+    expect(isIPv4Address('192.168.0.1')).toBe(true);
+    expect(isIPv4Address('10.0.0.1')).toBe(true);
+  });
+
+  test('should return false for invalid IPv4 address', () => {
+    expect(isIPv4Address('256.0.0.1')).toBe(false);
+    expect(isIPv4Address('192.168.0.256')).toBe(false);
+    expect(isIPv4Address('10.0.0.')).toBe(false);
+    expect(() => isIPv4Address('')).toThrow('Empty string');
+    expect(() => isIPv4Address()).toThrow('Undefined string');
+    expect(() => isIPv4Address({})).toThrow('Not a string');
+    expect(() => isIPv4Address([])).toThrow('Not a string');
+  });
+});
+
 
 
