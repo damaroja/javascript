59e967a294ea77a020e60b5e529be74daa8c9b03
damaroja <ildefonsoarrecife3@hotmail.com>
2024-03-12 20:37:38 +0000

    Update AnagramGroping.js+

--- Array/AnagramGroping.js
+++ Array/AnagramGroping.js
@@ -42,3 +42,30 @@ console.log(anagrams(['bat','rat']));
 //output: [['bat'], ['tar', 'rat'], ['cat'], ['mat', 'tam']]
 
 
+
+//****************
+test
+//****************
+
+
+const { anagrams } = require('./app');
+
+describe('anagrams', () => {
+  it('should return an array of anagram groups', () => {
+    const input = ['cat', 'tac', 'act', 'dog', 'god'];
+    const expectedOutput = [['cat', 'tac', 'act'], ['dog', 'god']];
+    expect(anagrams(input)).toEqual(expectedOutput);
+  });
+
+  it('should return an empty array if no anagrams are found', () => {
+    const input = ['hello', 'world', 'foo', 'bar'];
+    const expectedOutput = [['hello'], ['world'], ['foo'], ['bar']];
+    expect(anagrams(input)).toEqual(expectedOutput);
+  });
+
+  it('should handle empty input array', () => {
+    const input = [];
+    const expectedOutput = [];
+    expect(anagrams(input)).toEqual(expectedOutput);
+  });
+});
