59d93061d7e1c1d893b58ce0ab714f00b74334c1
damaroja <ildefonsoarrecife3@hotmail.com>
2024-03-12 12:58:21 +0000

    Create MaxSumKsubsetInArr.js

---
+++ Array/MaxSumKsubsetInArr.js
@@ -0,0 +1,19 @@
+
+
+
+//Create a function that takes an array of numbers and a k:integer as arguments. Must to return the maximum sum of k elements in the array.
+
+
+
+
+
+const maxSum = (arr, k) => {
+  const ArrOrdenado = arr.sort((a, b) => a - b);
+  return ArrOrdenado.reverse()
+    .slice(0, k)
+    .reduce((a, b) => a + b, 0);
+};
+
+module.exports = maxSum;
+
+console.log(maxSum([8, 2, 3, 6, 1], 3));
